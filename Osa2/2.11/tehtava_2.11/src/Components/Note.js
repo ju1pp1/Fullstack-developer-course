c55c6eb570a398aa5779e133f436ef4b5bbc55c3
ju1pp1 <jeremicraft@hotmail.com>
2022-09-19 17:44:34 +0300

    2.11 first

---
+++ Osa2/2.11/tehtava_2.11/src/Components/Note.js
@@ -0,0 +1,12 @@
+const Note = ({ note }) => {
+    
+    //{notes.map(x => 
+      //  <li key={x.id}> {x.content} </li>
+        //)}
+    return (
+        <div>
+        <li> {note.content} </li>
+            </div>
+    )
+}
+export default Note
---
+++ Osa2/2.11/tehtava_2.11/src/Components/Person.js
@@ -0,0 +1,11 @@
+const Person = ({ person }) => {
+    
+    //{notes.map(x => 
+      //  <li key={x.id}> {x.content} </li>
+        //)}
+    return (
+        
+        <p> {person.name} </p>
+    )
+}
+export default Person
