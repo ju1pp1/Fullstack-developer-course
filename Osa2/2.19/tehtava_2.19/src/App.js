f6813a713969578c2ac55365ae38e7bb0cdaff91
ju1pp1 <jeremicraft@hotmail.com>
2022-10-04 12:38:14 +0300

    Base

---
+++ Osa2/2.19/tehtava_2.19/src/App.js
@@ -0,0 +1,129 @@
+import {useState, useEffect} from 'react'
+import Note from './Components/Note'
+
+import noteService from './Services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
+    })
+}, [])
+
+  const toggleImportanceOf = id => {
+    //Jokaiselle muistiinpanolle id-kenttään perustuva yksilöivä url.
+    //const url = `http://localhost:3001/notes/${id}`
+    //Taulukon find-metodilla etsitään muutettava muistiinpano ja talletetaan muuttujaan -note- viite siihen. 
+    const note = notes.find(n => n.id === id)
+    //Luodaan uusi olio, jonka sisältö on sama kuin vanhan olion sisältö. Poislukien kenttä important.
+    const changedNote = {...note, important: !note.important}
+
+    /*
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+    */
+  noteService
+  .update(id, changedNote).then(returnedNote => {
+    setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+  })
+  .catch(error => {
+    alert(
+      `the note '${note.content}' was already deleted from server`
+    )
+    setNotes(notes.filter(n => n.id !== id))
+  })
+}
+const deleteObjectOf = id => {
+  const deleteNote = notes.find(n => n.id === id)
+  const deletedNote = {...deleteNote}
+
+  const confirm = window.confirm(`Are you sure?`)
+  if(confirm) {
+    noteService
+    .deleteThis(id, deletedNote).then(deletednote => {
+      //setNotes(notes.map(note => note.id ? note : deletednote))
+      setNotes(notes.filter(n => n.id !== id))
+    })
+    .catch(error => {
+      alert(
+        `the user '${deleteNote.name}' has already been deleted from the server.`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })
+  } else {
+  
+}
+}
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      name: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+    }
+    /*
+    axios
+          .post('http://localhost:3001/notes', noteObject)
+          .then(response => {
+            setNotes(notes.concat(response.data))
+            setNewNote('')
+          }) 
+    */
+  noteService
+  .create(noteObject)
+  .then(returnedNote => {
+    setNotes(notes.concat(returnedNote))
+    setNewNote('')
+  })
+}
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important)
+
+  return (
+      <div>
+        <h1>Phonebook</h1>
+        
+       <form onSubmit={addNote}>
+        <div>
+        Name: <input 
+        value={newNote}
+        onChange={handleNoteChange}
+        />
+        </div>
+        <div>
+        <button type="Submit">Save</button>
+        </div>
+       </form>
+
+       <div>
+        <h2>Numbers</h2>
+          
+        <ul>
+          {notesToShow.map(note =>
+            <Note key={note.id} note={note} deleteObject={() => deleteObjectOf(note.id) } toggleImportance={() => toggleImportanceOf(note.id)} />
+       )}       
+       </ul>
+
+         <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all' }
+          </button>  
+          
+        </div>
+
+      </div>
+  )
+}
+export default App
---
+++ Osa2/2.19/tehtava_2.19/src/Components/Note.js
@@ -0,0 +1,11 @@
+const Note = ({ note, toggleImportance, deleteObject }) => {
+    
+    const label = note.important ? 'make not important' : 'make important'
+    return (
+        <div>
+        <li> {note.content} {note.name} {note.phone}<button onClick={toggleImportance} >{label} </button>
+            <button onClick={deleteObject} >Delete</button> </li>
+            </div>
+    )
+}
+export default Note
